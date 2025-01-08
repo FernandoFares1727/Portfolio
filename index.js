88cf2856ca685b67a2b1cb1933a0d19e6d3cf620
FernandoFares1727 <fernandimfares@gmail.com>
2025-01-08 19:10:33 -0300

    Change view

---
+++ index.js
@@ -0,0 +1,35 @@
+
+var currentPage = "";
+
+// Seleciona o elemento que contém o conteúdo dinâmico
+const content = document.querySelector('.bodyContent');
+
+// Função para atualizar o conteúdo
+function updateContent(page) {
+
+    if(page == currentPage)
+        return;
+
+    // Simulação de requisição para buscar o conteúdo (substitua por sua lógica real)
+    if (page === 'home') {
+      fetch('home/index.html')
+      .then(response => response.text())
+      .then(data => {
+        currentPage = page;
+        content.innerHTML = data;
+      })
+    } 
+}
+
+// Chama a função para carregar a página Home inicialmente
+updateContent('home');
+
+// Adiciona um event listener aos links da navegação
+const links = document.querySelectorAll('nav a');
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const page = link.dataset.page;
+    updateContent(page);
+  });
+});
