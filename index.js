df2e0cc2ac126b355c9d2ca2b41cd1f3af4ba93c
FernandoFares1727 <fernandimfares@gmail.com>
2025-01-08 20:01:14 -0300

    Change page flow

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ const content = document.querySelector('.bodyContent');
 
 // Função para atualizar o conteúdo
 function updateContent(page) {
-
+  
     if(page == currentPage)
         return;
 
-    // Simulação de requisição para buscar o conteúdo (substitua por sua lógica real)
-    if (page === 'home') {
-      fetch('home/index.html')
-      .then(response => response.text())
-      .then(data => {
-        currentPage = page;
-        content.innerHTML = data;
-      })
-    } 
-}
+   fetch(`${page}/index.html`)
+   .then(response => response.text())
+   .then(data => {
+       currentPage = page;
+       content.innerHTML = data;
+   })
+ } 
 
 // Chama a função para carregar a página Home inicialmente
 updateContent('home');
